73958533c98ae39c1a37f499c2a8d4c5874aa45c
OfficalDivinity <officalchaoschamp@gmail.com>
2014-11-16 19:57:29 +0000

    Update BaseClass.java

--- Magicnation/com/juanwan11_magicnation/BaseClass.java
+++ Magicnation/com/juanwan11_magicnation/BaseClass.java
@@ -51,6 +51,8 @@ public class BaseClass {
 		    MABotaniaItems.registerItems();
 		}
 		if (isThaumcraftLoaded) {
+			MAThaumBlocks.makeBlocks();
+		        MAThaumBlocks.registerBlocks();
 		}
 	
 	}
