701275d4472161dae85fb86dc6e47db5313c1b78
OfficalDivinity <officalchaoschamp@gmail.com>
2014-11-28 22:07:01 +0000

    Update MABotaniaItems.java

--- Magicnation/com/juanwan11_magicnation/Items/MABotaniaItems.java
+++ Magicnation/com/juanwan11_magicnation/Items/MABotaniaItems.java
@@ -1,5 +1,18 @@
 package com.juanwan11_magicnation.Items;
 
 public class MABotaniaItems {
+  public static itemManaGemStoneBloodPickaxe;
+  
+  	public static void makeItems() {
+	
+		itemManaGemStoneBloodPickaxe = new itemManaGemStoneBloodPickaxe(ToolMaterial.EMERALD).setUnlocalizedName(ModInformation.modid + "_" + "itemGemStoneBloodPickaxe").setTextureName(ModInformation.modid + ":" + "itemGemStoneBloodPickaxe").setCreativeTab(BaseClass.magicnationTab).setMaxStackSize(1);
+	
+	};
+
+	public static void registerItems() {
+
+		GameRegistry.registerItem(itemManaGemStoneBloodPickaxe, "itemManaGemStoneBloodPickaxe");
+
+	};
 
 }
