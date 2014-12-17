5e1e3c66c6a8d9664d5de2bb9dbc5291de3ec695
Devilin Pixy <daimyholster@live.nl>
2014-12-17 17:55:26 +0100

    Update WorldGenStarterStructure.java

--- Magicnation/com/juanwan11_magicnation/structures/WorldGenStarterStructure.java
+++ Magicnation/com/juanwan11_magicnation/structures/WorldGenStarterStructure.java
@@ -7,14 +7,24 @@ import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class WorldGenStarterStructure extends WorldGenerator {
-	public boolean generate(World world, Random random, int blockx, int blocky, int blockz) {
 
-		while (world.isAirBlock(blockx, blocky, blockz) && blocky > 2) {
-			--blocky;
+	public boolean generate(World world, Random random, int x, int y, int z) {
+
+		while (world.isAirBlock(x, y, z) && y > 2) {
+			--y;
 		}
 
-		world.setSpawnLocation(blockx, blocky, blockz);
+		world.setSpawnLocation(x, y, z);
 
+		//Array list containing x,y,z multiples
+		int[] coords = {0,0,0,1,0,0,2,0,0}
+		for (int i = 0; i < coords.length/3; i=+3) {
+			int x = coords[i];
+			int y = coords[i+1];
+			int z = coords[i+2];
+			world.setBlock(x, y, z, Blocks.stonebrick);
+		}
+		
 		return true;
 	}
 }
