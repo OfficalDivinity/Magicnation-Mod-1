b8788e153f3f86ee8048b8cb43468db7addf9a3f
almlk1996 <moris.rafol@gmail.com>
2014-12-19 23:22:48 +0200

    Update WorldGenStarterStructure.java

--- Magicnation/com/juanwan11_magicnation/structures/WorldGenStarterStructure.java
+++ Magicnation/com/juanwan11_magicnation/structures/WorldGenStarterStructure.java
@@ -20,35 +20,7 @@ public class WorldGenStarterStructure extends WorldGenerator {
 		return true;
 	}
 	
-	public void box(World world,int posx, int posy, int posz,int x, int y, int z) {
-		
-		int j,k;
-	
-		//build walls 
-		for(j=0;j<x;j++)
-			for(k=0;k<y;k++){
-				world.setBlock(posx +j,posy +k,posz, Blocks.stonebrick);
-				if(j==1)continue;
-				world.setBlock(posx +j,posy +k,posz +z, Blocks.stonebrick);
-			}
 
-		for(j=1;j<y;j++)
-			for(k=0;k<y;k++){
-				world.setBlock(posx,posy +j,posz +k, Blocks.stonebrick);
-				if(j==(y-1))continue;
-				world.setBlock(posx +x,posy +j,posy +k, Blocks.stonebrick);
-			}
-		
-		for(j=1;j<z-1;j++)
-			for(k=1;k<x-1;k++){
-				world.setBlock(posx, posy,posz +z, Blocks.stonebrick);
-				world.setBlock(posy +k,posy +y,posy +z, Blocks.stonebrick);
-			}
-		
-		
-		
-		
-	}
 	
 	
 }
